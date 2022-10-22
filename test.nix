c346e94fa385acc1116ef87dd317ee2e99a6ea13
Jade Lovelace <software@lfcode.ca>
2022-10-22 03:21:14 -0700

    test file

---
+++ test.nix
@@ -0,0 +1,20 @@
+# a test derivation that just sleeps a little
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.stdenv.mkDerivation {
+    unpackPhase = ''
+      sleep 0.5
+    '';
+    name = "test-0.0.0";
+    configurePhase = ''
+      sleep 0.5
+    '';
+    buildPhase = ''
+      sleep 0.5
+    '';
+    installPhase = ''
+      sleep 0.2
+      mkdir $out
+    '';
+  }
